6a4bdf42fe0e3f4c6028a8b38d57a36378c1b7fb
Sven <svnerskoglund@gmail.com>
2018-02-26 14:38:04 -0700

    drill 1

--- Java/jfop/screenoutputkeyboardinput/drills/Formatting.java
+++ Java/jfop/screenoutputkeyboardinput/drills/Formatting.java
@@ -3,13 +3,13 @@ public class Formatting {
 
 	public static void main(String[] args) {
 		/*Change the String literal to print
-		  
-		  \\ His name was "Robert Paulson."  
+
+		  \\ His name was "Robert Paulson."
 		  You cry now. //
-		    
+
 		  Make sure it is a single String literal.
 		 */
-		System.out.println("His name was Robert Paulson. You cry now.");
+		System.out.println("\\\\His name was \"Robert Paulson.\" \nYou cry now. //");
 	}
 
 }
