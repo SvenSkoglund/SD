9196e8b62de976db367e2e839797aa13d9a6465f
Sven <svnerskoglund@gmail.com>
2018-02-26 14:10:45 -0700

    PrintHeader

---
+++ Java/jfop/introtomethods/Labs/PrintHeader.java
@@ -0,0 +1,14 @@
+public class PrintHeader {
+  public static void main (String [] args){
+    printTheHeader();
+
+
+  }
+  public static void printTheHeader(){
+    System.out.println("*****************");
+    System.out.println("*               *");
+    System.out.println("*   Welcome!    *");
+    System.out.println("*               *");
+    System.out.println("*****************");
+  }
+}
