dd3f5e5f7a454906aac2b93d3773f326eb1cc5b0
Sven Skoglund <svenrskoglund@gmail.com>
2018-02-28 08:00:03 -0700

    morning while loop drills

--- Java/workspace/WhileLoops/src/drills/WhileLoopSteps.java
+++ Java/workspace/WhileLoops/src/drills/WhileLoopSteps.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class WhileLoopSteps {
 
-  public static void main(String[] args) {
-    // Write a `while` loop that will accept a number from the user, multiply it by
-    // 2, and print the value to the screen. The program should accept data from the
-    // user until the user enters zero.
-    Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
+	public static void main(String[] args) {
+		// Write a `while` loop that will accept a number from the user, multiply it by
+		// 2, and print the value to the screen. The program should accept data from the
+		// user until the user enters zero.
+		Scanner sc = new Scanner(System.in);
 
-    // while loop to multiply and get more input...
-  }
+		int num = 1;
+		while (num != 0) {
+			System.out.print("Enter a number to be multiplied by 2. Enter 0 to quit: ");
+			num = sc.nextInt();
+			System.out.println(num + " multiplied by 2 equals " + (num*2));
 
+			// while loop to multiply and get more input...
+		}
+
+	}
 }
