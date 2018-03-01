88cf25579f438d10468b778ec833124628a0e36e
Sven Skoglund <svenrskoglund@gmail.com>
2018-03-01 11:39:54 -0700

    Drills on objects :+1:

--- Java/workspace/IntroToObjects/src/drills/SphereReferences.java
+++ Java/workspace/IntroToObjects/src/drills/SphereReferences.java
@@ -2,19 +2,24 @@ package drills;
 
 public class SphereReferences {
 
-  public static void main(String[] args) {
-    Sphere sphRef;
-    sphRef = new Sphere();
-    sphRef.radius = 10.0;
-    System.out.println("Original sphRef radius: " + sphRef.radius);
-    // Create another reference called otherSphereRef and assign sphRef to it,
-    // so that both references refer to the same Sphere object.
+	public static void main(String[] args) {
+		Sphere sphRef;
+		sphRef = new Sphere();
+		sphRef.radius = 10.0;
+		System.out.println("Original sphRef radius: " + sphRef.radius);
+		// Create another reference called otherSphereRef and assign sphRef to it,
+		// so that both references refer to the same Sphere object.
+		Sphere otherSphereRef = sphRef;
+		otherSphereRef.radius = 20;
+		System.out.println("New otherSphereRef radius: " + otherSphereRef.radius);
 
-    // Change otherSphereRef's radius.
+		System.out.println("New sphRef radius: " + sphRef.radius);
 
-    // Output otherSphereRef's radius to the screen.
+		// Change otherSphereRef's radius.
 
-    // Output sphRef's radius to the screen. It should have changed.
-  }
+		// Output otherSphereRef's radius to the screen.
+
+		// Output sphRef's radius to the screen. It should have changed.
+	}
 
 }
--- Java/workspace/IntroToObjects/src/drills/SphereWithColor.java
+++ Java/workspace/IntroToObjects/src/drills/SphereWithColor.java
@@ -2,13 +2,16 @@ package drills;
 
 public class SphereWithColor {
   public double radius;
+  public String color;
   // Add a String field for color.
 
   public double getVolume() {
     double vol = 4.0 / 3.0 * 3.14159 * radius * radius * radius;
     return vol;
   }
-
+  public void displaySphere() {
+	  System.out.println("Radius: " + radius +"\nVolume: " + getVolume() + "\nColor: " + color);
+  }
   // Add a method called displaySphere.
   // return type: void
   // parameters: none
