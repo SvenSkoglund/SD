8fffea9989b1401e9f95e55d050f80d47c0782cf
Sven Skoglund <svenrskoglund@gmail.com>
2018-03-13 18:08:02 -0600

    Added NameSorting Lab

--- Java/workspace/MapInterface/src/com/skilldistillery/mapinterface/labs/MapCollectionExample.java
+++ Java/workspace/MapInterface/src/com/skilldistillery/mapinterface/labs/MapCollectionExample.java
@@ -1,29 +1,45 @@
 package com.skilldistillery.mapinterface.labs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class MapCollectionExample {
 
-	public static void main(String[] args) {
-		Map <Character,String> letterToName= new HashMap<>();
-		String name = "Amanda";
-		Character firstChar = name.charAt(0);
-		letterToName.put(firstChar, name);
-		
-		System.out.println(letterToName);
-		
-		Map <Character,Set<String>> letterToManyNames = new HashMap<>();
-		String name2 = "Alan";
-		Character firstChar2 = name.charAt(0);
-		Set <String> names = new HashSet<>();
-		letterToManyNames.put(firstChar, names);
-		letterToManyNames.put(firstChar2, names);
+	public static void main(String[] args) throws IOException {
+		// Create tree set to hold sorted names
+		Set<String> names = new TreeSet<>();
 
-		System.out.println(letterToManyNames);
-		
+		// Create map to store character keys that point names starting at that char
+		Map<Character, Set> map = new HashMap<>();
+
+		// adding file names.txt and storing to f
+		File f = new File("names.txt");
+
+		// create file/buffer reader to interact with names.txt
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+
+		// create loop
+		String line;
+		// check if buffered reader line is not null, execute the loop
+		while ((line = br.readLine()) != null) {
+			// adding line from buffer reader in to generic names tree set
+			names.add(line);
+			// check to see if map at next char has value or not
+			if (map.get(line.charAt(0)) == null) {
+				// if no tree set for first letter of name exists, create new tree set and
+				// assign first char as its key
+				map.put(line.charAt(0), new TreeSet<>());
+			} // adding name to the tree set corresponding to first letter
+			map.get(line.charAt(0)).add(line);
+
+		}
+		System.out.println(map);
 	}
-
 }
